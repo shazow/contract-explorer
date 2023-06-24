8f712c64a5b6735aa10f4564b333a250515bf90f
Andrey Petrov <andrey.petrov@shazow.net>
2023-06-24 10:54:25 -0400

    Merge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,40 @@
 <script lang="ts">
-  function onSubmit(e) {
-    const formData = new FormData(e.target);
+    import { whatsabi } from "@shazow/whatsabi";
+    import { ethers } from "ethers";
 
-    const data = {};
-    for (let field of formData) {
-      const [key, value] = field;
-      data[key] = value;
+    const provider = new ethers.providers.InfuraProvider("homestead", "22af2ac3832349cfb8b5c9e6d3b6197b");
+
+    let loading = false;
+    let address = "";
+    let abi = [];
+
+    function loadABI(a) {
+        if (loading) return;
+        loading = true;
+        address = a;
+        console.log("Loading address:", address);
+        whatsabi.autoload(address, {
+            provider,
+            abiLoader: new whatsabi.loaders.EtherscanABILoader(),
+            signatureLookup: new whatsabi.loaders.OpenChainSignatureLookup(),
+        }).then((result) => {
+            abi = result;
+        }).finally(() => {
+            loading = false;
+        });
     }
-    console.log(data)
-  }
+
+    function onSubmit(e) {
+        const f = new FormData(e.target);
+        const a = f.get("address") || "";
+        if (!a) return;
+
+        loadABI(a);
+    }
+
+    function searchAddress(e) {
+        loadABI(e.target.innerText);
+    }
 </script>
 
 
@@ -18,16 +44,23 @@
     <p>Like Internet Explorer, but for contracts.</p>
 </header>
 
-<form class="" on:submit|preventDefault={onSubmit}>
+<form class="center" on:submit|preventDefault={onSubmit} disabled="{!loading}">
     <div class="search">
-        <input type="text" class="searchTerm" placeholder="> contract address">
+        <input type="text" class="searchTerm" name="address" value={address} placeholder="> contract address">
         <button type="submit" class="searchButton">Search</button>
     </div>
 </form>
+{#if loading}
+
+<div class="throbber" >
+    Loading...
+</div>
+{/if}
+
 
 <div class="center examples-text">
     <h2>Examples</h2>
-    <p>Nouns: 0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03</p>
+    <p>Nouns: <a on:click={searchAddress}>0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03</a></p>
     <p>MakerDAO:</p>
     <p>ApeCoin:</p>
     <p>Random unverified contract:</p>
@@ -46,6 +79,10 @@
     </form>
 
 </div>
+
+<pre class="abi-dump">
+    {JSON.stringify(abi)}
+</pre>
 
 <style lang="scss">
 
