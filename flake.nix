5f1a15ec2f2b8ab3a8a5d7a53509a89bc4341e79
Andrey Petrov <andrey.petrov@shazow.net>
2023-06-24 09:25:27 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_20
+        ];
+
+        shellHook = ''
+          export PS1="[dev] $PS1"
+          export PATH=$PWD/node_modules/.bin:$PATH
+
+          [[ -f .env ]] && source .env
+        '';
+      };
+    });
+}
