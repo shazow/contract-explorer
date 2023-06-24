9ffa6b4293d1ff3f37e836d23e0177f411558e06
Andrey Petrov <andrey.petrov@shazow.net>
2023-06-24 16:10:57 -0400

    Add signature table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,6 +32,7 @@
             //}
             return;
         }
+        typedWidget = false;
         console.log("unknown type, no specialized widget yet");
     }
 
@@ -41,7 +42,13 @@
         address = a;
         console.log("Loading address:", address);
         const codeLoader = new whatsabi.loaders.EtherscanABILoader({ apiKey: "SHT8M9JSGR62U5U7YVFUSTPG41IVR1F7ND" });
-        abi = await codeLoader.loadABI(address);
+        let r;
+        try {
+            r = await codeLoader.loadABI(address);
+        } catch (e) {
+            r = [];
+        }
+        abi = r;
         if (abi.length > 0) {
             detectWidget(abi);
             loading = false;
@@ -51,12 +58,12 @@
         const code = await provider.getCode(address);
         const selectors = whatsabi.selectorsFromBytecode(code);
         const signatureLookup = new whatsabi.loaders.OpenChainSignatureLookup();
-        selectors.map((selector) => {
-            const signature = signatureLookup.loadFunctions(selector);
+        for (const selector of selectors) {
+            const signature = await signatureLookup.loadFunctions(selector);
             if (signature.length > 0) {
                 abi.push(signature[0]);
             }
-        });
+        };
 
         loading = false;
         detectWidget(abi);
@@ -73,6 +80,18 @@
 
     function searchAddress(e) {
         loadABI(e.target.innerText);
+    }
+
+    function toReadableFunctions(a) {
+        return a.filter((el) => {
+            return typeof(el) === "string" || el.type === "function";
+        }).map((el) => {
+            return ethers.utils.FunctionFragment.from(el).format();
+        });
+    }
+
+    function copyToClipboard(e) {
+        alert("Lets pretend we copied to clipboard");
     }
 </script>
 
@@ -101,8 +120,8 @@
 <div class="examples-text">
     <h2>Examples</h2>
     <ul>
-        <li>Nouns: <a on:click={searchAddress}>0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03</a></li>
-        <li>ApeCoin: <a on:click={searchAddress}>0x4d224452801aced8b2f0aebe155379bb5d594381</a></li>
+        <li>Nouns NFT: <a on:click={searchAddress}>0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03</a></li>
+        <li>ApeCoin ERC-20: <a on:click={searchAddress}>0x4d224452801aced8b2f0aebe155379bb5d594381</a></li>
         <li>Rando Unverified Contract: <a on:click={searchAddress}>0xab4f32686cf8687d881462fe29170fb17180155b</a></li>
     </ul>
 </div>
@@ -110,30 +129,17 @@
 <svelte:component this={typedWidget} {...widgetArgs} />
 
 {#if abi.length > 0}
-<div class="content-container abi-dump">
-<code>
-    <button onclick="myFunction()" class="button">Copy text</button>
-    {JSON.stringify(abi)}
+<ul class="abi-functions">
+    {#each toReadableFunctions(abi) as sig, i}
+    <li>{sig}</li>
+    {/each}
+</ul>
 
-    <!-- <table>
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Payable</th>
-                <th>State Mutability</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each abi as selector, i}
-                <tr>
-                    <td class="border border-slate-300">{abi[i].type}</td>
-                    <td class="border border-slate-300">{abi[i].payable}</td>
-                    <td class="border border-slate-300">{abi[i].stateMutability}</td>
-                </tr>
-            {/each}
-        </tbody>
-    </table> -->
-</code>
+<div class="content-container abi-dump">
+    <code>
+        <button onclick="copyToClipboard()" class="button">Copy text</button>
+        {JSON.stringify(abi)}
+    </code>
 </div>
 {/if}
 
